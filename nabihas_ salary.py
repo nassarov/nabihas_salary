a9892693287729d667fc04d3a376f0d93e23848b
Ali Nassar <aliinassar8@gmail.com>
2025-02-21 15:10:34 +0200

    Creating a dictionary so our program expect from user to enter the number of the month instead of name

--- nabihas_ salary.py
+++ nabihas_ salary.py
@@ -1,3 +1,17 @@
 # Finance Summary Program
 salary = float(input("Please enter your salary : "))
 month =  input("Please enter the name of the month: ")
+months_dict = {
+    "1": "January",
+    "2": "February",
+    "3": "March",
+    "4": "April",
+    "5": "May",
+    "6": "June",
+    "7": "July",
+    "8": "August",
+    "9": "September",
+    "10": "October",
+    "11": "November",
+    "12": "December",
+}
